9c2b4db0e5bdd40e1a0047a5d4eb8309499838dc
230208724 <l1256184551@163.com>
2023-04-27 23:50:39 +0800

    stage2 accomplished

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,154 @@
-/* example
-let myHeading = document.querySelector('h1');
-if (myHeading.textContent === 'Hello world!') {
-  alert('测试阶段');
-} else {
-  alert(myHeading.textContent);
-}
-*/
+//const jsPDF = require('jspdf')
+//import { jsPDF } from './jspdf.umd.min.js';
+alert('importing');
+//let jsPDF;
+//const html2pdf = require('html2pdf');
 
 
-/*
-document.querySelector('html').addEventListener('click', () => {
-    alert('click 测试');
-  });
- */
+// set user
+localStorage.clear();
 
-let myImage = document.querySelector('img');
-myImage.onclick = function(){
-    let mySrc = myImage.getAttribute('src');
-    let myAlt = myImage.getAttribute('alt');
-    let myCls = myImage.getAttribute('class');
-    if(mySrc.endsWith('.png')){
-        alert(myCls + myAlt);
-        myImage.setAttribute('src', 'images/icon.jpg');
-        
-    } else {
-        alert('iclass 测试');
-    }
-} 
- 
-let myHeading = document.querySelector('h5');
+let myHeading = document.querySelector('#hint');
+let Heading = document.querySelector('.UserButton');
+
 function setUserName() {
-let myName = prompt('User: ');
-if(!myName) {
-    setUserName();
-} else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Dear ' + myName;
-}
-}
+    let myName = prompt('input report name such as 李莹鑫-20230404: ');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = myName+'\'s WorkReport';
+        Heading.textContent = myName;
+    }
+    }
 
-if(!localStorage.getItem('name')) {
+if(!localStorage.getItem('name')) {//this item will be used to be the file's name
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Dear ' + storedName;
+    myHeading.textContent = storedName+'\'s WorkReport';
+    Heading.textContent = myName;
+}
+alert('name set as '+localStorage.getItem('name'))
+
+//response to user button
+Heading.addEventListener("click", setUserName);
+
+// response to delete button
+
+
+//response to edit button
+function createNewPage(module,moduleid,moduledes) {
+    localStorage.setItem('curproject',module)
+    localStorage.setItem('curprojectnumber',moduleid)
+    localStorage.setItem('curprojectdes',moduledes)
+    alert('cur project is '+localStorage.getItem('curproject'))
+    window.open("./" + module + ".html"); 
+  }
+
+//response to image
+let mos = document.querySelector('div');
+let e = 0;
+
+function addModule(alt) {
+    e += 1;
+    let des = prompt('input your description of module '+e.toString()+' '+alt);
+    let mo = document.createElement('p');
+    mo.setAttribute('class',alt);  
+    mo.setAttribute('id',e.toString());
+    mo.textContent = des + '\n';
+    mos.appendChild(mo);  
+    let edit = document.createElement('button');
+    edit.setAttribute('class','EditButton');
+    edit.setAttribute('id',e.toString());
+    edit.textContent = 'edit';
+    mos.appendChild(edit);
+    edit.onclick = function(){
+        createNewPage(alt,e.toString(),des)
+    }
+    let delet = document.createElement('button');
+    delet.setAttribute('class','DeleteButton');
+    delet.setAttribute('id',e.toString());
+    delet.textContent = 'delete';
+    mos.appendChild(delet);
+    delet.onclick = function(){
+        deleteElement(alt,e.toString(),des)
+    }
+}
+alert('set div append rule')
+
+
+let Images = document.querySelectorAll('.ModuleImage');
+
+for(let i = 0; i < Images.length ; i++) {
+    let imgalt = Images[i].getAttribute('alt');
+    //let imgid = Images[i].getAttribute('id');
+    Images[i].onclick = function(){
+        addModule(imgalt);
+    } 
 }
 
-let myButton = document.querySelector('button');
-myButton.onclick = function() {
-    setUserName();
- }
+  
+// response to review buttons
+let items = ['motivation','acomplishment','problem','course','todo'];
+function reviewData() {    
+    //alert('test on 1: ' + localStorage.getItem('1motivation'))
+    const doc = new jsPDF(); // Create new PDF document // or var
+    //alert('test on 2: ' + localStorage.getItem('2motivation'))
+    for(let i = 0; i < e ; i++) {
+        for(let j = 0; j<items.length; j++){
+            k = i.toString() + items[j]
+            v = sessionStorage.getItem(k)
+            if (v){
+                doc.text(k + " :\n" + v, 10, 10);
+            }
+        }     
+    }   
+    doc.save("./data.pdf"); // Download PDF
+    window.close();
+  }
+// function reviewData() {    
+//     alert('test ' + e.toString() + localStorage.getItem('1motivation'))
+//     alert('test ' + e.toString() + localStorage.getItem('2motivation'))
+//     const options = {
+//         filename: 'google.pdf',
+//         html2canvas: { scale: 2 },
+//         jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+//         margin: { top: 0.5, right: 0.5, bottom: 0.5, left: 0.5 },
+//         enableLinks: true,
+//         pageTitle: 'Google'
+//     };
+    
+//     const element = document.getElementById('google');
+    
+//     html2pdf().from(element).set(options).save();    
+// }
+document.getElementById("ReviewButton").addEventListener("click", reviewData);
 
+
+// response to export buttons
+
+
+
+
+// // Function to show input page and hide original page
+// function showInputPage() {
+//     document.getElementById("write-button").style.display = "none";
+//     document.getElementById("input-page").style.display = "block";
+//   }
+  
+//   // Function to show review button and hide input page
+//   function showReviewButton() {
+//     document.getElementById("input-page").style.display = "none";
+//     document.getElementById("review-button").style.display = "block";
+//   }
+
+
+// const form = document.querySelector('#key-value-form');
+// const submitButton = document.querySelector('#submit-button');
+
+
+//// Convert the key-value pairs to an array of HTML list items
+// const keyValueListItems = Object.entries(keyValues).map(([key, value]) 
 
   
